8f36e5f7613e332a5ef281f29b6d39391017085f
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-03-30 20:01:32 -0300

    agregando boton para cambiar a modo oscuro

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector('.logos').appendChld(copy);
+
+const body = document.querySelector("body");
+const button = document.getElementsById("button-element");
+        
+button.onclick = function() {
+    button.classList.toggle('active');
+    body.classList.toggle('active');
+}
