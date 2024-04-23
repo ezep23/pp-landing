ee875b68a84a35c453d1652d5c69d9c2522fe43d
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-04-23 00:26:43 -0300

    arreglar el boton

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.querySelector('.logos').appendChld(copy);
 const body = document.querySelector("body");
 const button = document.getElementsById("button-element");
         
-button.onclick = function() {
-    button.classList.toggle('active');
-    body.classList.toggle('active');
-}
+button.addEventListener("click", changeColor(
+    button.classList.toggle('active'),
+    body.classList.toggle('active')
+));
+
