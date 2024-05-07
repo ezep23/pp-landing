c043bcdf7d3b7edd2823b30231564a33006c7339
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-05-07 20:24:49 -0300

    eliminación del boton dark mode y deploy

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
+/* Pertenece al slider de tecnologías */
 const copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector('.logos').appendChld(copy);
-
-const body = document.querySelector("body");
-const button = document.getElementsById("button-element");
-        
-button.addEventListener("click", changeColor(
-    button.classList.toggle('active'),
-    body.classList.toggle('active')
-));
-
