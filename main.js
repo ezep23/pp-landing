ad7df7b0477b9fc82687b189fadb16c4cdaab19e
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-10-28 13:25:18 -0300

    cto button & gitignore

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-/* Pertenece al slider de tecnologías */
+
+function goHero(){
+    window.scrollTo({
+        top: 800,
+        behavior: 'smooth',   
+    });
+}
+
 const copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector('.logos').appendChld(copy);
